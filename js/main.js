973b99b34fb2368b923f253e398943a0afd2a249
MarcosVini9999 <marcosviniciusandradedesousa@hotmail.com>
2022-05-31 20:18:45 -0300

    Loading screen increment

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
+//Declarando variável
 let btnContact = document.querySelector(".jl-btn-contact");
 
+//Tela de loading
+window.addEventListener("load", function () {
+  let pagePreloader = document.querySelector(".jl-preloader");
+
+  setTimeout(function () {
+    pagePreloader.classList.add("jl-fade-out");
+    pagePreloader.style.display = "none";
+  }, 4000);
+});
+
+//Lógica do botão decontato
 btnContact.addEventListener("click", function () {
   let boxContact = document.querySelector(".jl-contact-info");
 
